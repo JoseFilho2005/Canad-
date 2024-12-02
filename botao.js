b69fab68e7ed60a672c26633dfc2a2d86d421bc5
JoseFilho2005 <joseads2005@gmail.com>
2024-12-02 00:36:32 -0300

    botao.js

---
+++ botao.js
@@ -0,0 +1,11 @@
+const botao = document.getElementById('meuBotao');
+const frase = document.getElementById('minhaFrase');
+
+
+<p> Prof deu tuso errado e tive que imp0lementar ele diretamente no  "beleza.html"<p/>
+
+// Adiciona um event listener ao botão
+botao.addEventListener('click', () => {
+  // Oculta o parágrafo
+  frase.style.display = 'none';
+});
